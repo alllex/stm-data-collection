83d207f0328868d098341210b7295a4b7bf43fce
Aleksey Semin <alllex.semin@gmail.com>
2015-07-19 01:20:01 +0300

    Added: clean up after benchmark

--- priority-queue/onebench.sh
+++ priority-queue/onebench.sh
@@ -5,6 +5,11 @@ if [ $? -eq 0 ]; then
   echo OK
   # ./Bench timing 100000 200 --runs=50 --initsize=10000 --insrate=50 +RTS -s -N4 -la -K16m -qa
   ./Bench throughput 200 --runs=3 --initsize=10000 --insrate=50 +RTS -s -N4 -la -K16m -qa
+
+  rm Bench
+  rm *.o *.hi
+  rm Internal/*.o Internal/*.hi
+  rm ../*.o ../*.hi
 else
   echo FAIL
 fi
