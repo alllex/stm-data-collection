ec20966ab3672a3617e6ecb108f1aa4edecc9cd5
Aleksey Semin <alllex.semin@gmail.com>
2015-10-22 12:07:53 +0300

    Fixed: escape character in code in docs

--- src/Data/STM/PriorityQueue/Class.hs
+++ src/Data/STM/PriorityQueue/Class.hs
@@ -20,7 +20,7 @@ main :: IO ()
 main = do
     pq <- atomically $ (PQ.new :: STM (PQ.Impl Int Int))
     let kvs = [(2, 1), (5, 3), (1, 2), (4, 5)]
-    forM_ kvs $ \(k, v) -> atomically $ PQ.insert pq k v
+    forM_ kvs $ \\(k, v) -> atomically $ PQ.insert pq k v
     x <- atomically $ PQ.deleteMin pq
     putStrLn $ "x = " ++ show x -- prints 2
 @
