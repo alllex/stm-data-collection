d6e11b893ea14c9638e70459226bd8791ec041c1
Aleksey Semin <alllex.semin@gmail.com>
2015-10-22 12:50:30 +0300

    Fixed: fresh-clone script

--- utils/after-fresh-clone.sh
+++ utils/after-fresh-clone.sh
@@ -2,21 +2,16 @@
 # run from project root as `./utils/after-fresh-clone.sh`
 
 echo --------------------------------------
-echo Updating git submodules
+echo Creating sandboxes
 echo --------------------------------------
-git submodule init && git submodule update
+cabal sandbox init --sandbox . 
 
-if [ $? -eq 0 ]; then
-    echo --------------------------------------
-    echo Creating sandboxes
-    echo --------------------------------------
-    cabal sandbox init --sandbox . 
-    cabal sandbox add-source pkgs/pcg-random
+echo --------------------------------------
+echo Installing dependencies
+echo --------------------------------------
+cabal install -j --only-dependencies --enable-tests --enable-benchmarks
 
-    echo --------------------------------------
-    echo Installing dependencies
-    echo --------------------------------------
-    cabal install -j --only-dependencies --enable-tests --enable-benchmarks
-else
-    echo FAILED to fetch submodules
-fi
+echo --------------------------------------
+echo Testing 
+echo --------------------------------------
+cabal configure && cabal test
